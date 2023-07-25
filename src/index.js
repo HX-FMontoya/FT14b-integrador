dd8b53e2f7243fb0fa55e9ef0d9cde9e37fb4856
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-07-24 20:49:52 -0500

    vinculando

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const saludar = {
+  age: 8,
+};
+ReactDOM.render(
+  <App name="Feli" saludar={saludar} />,
+  document.getElementById("root")
+);
